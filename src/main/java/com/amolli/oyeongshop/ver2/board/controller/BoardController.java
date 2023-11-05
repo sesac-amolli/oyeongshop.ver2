c648b21acbc5506806f15538b9f1e4f296e84564
nayeong <nykim1102@gmail.com>
2023-11-06 01:39:37 +0900

    design,add board views ê²상품게ìë¬전체적ì

--- src/main/java/com/amolli/oyeongshop/ver2/board/controller/BoardController.java
+++ src/main/java/com/amolli/oyeongshop/ver2/board/controller/BoardController.java
@@ -7,9 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-    @GetMapping("/QnA")
-    public String index( ) {
-        return "/board/qna-list";
+    @GetMapping("/lists")
+    public String qnaList( ) {
+        return "/board/qna-product-list";
     }
+
+    @GetMapping("/write")
+    public String qnaWrite() { return "/board/qna-product-write"; }
+
+    @GetMapping("/pwd")
+    public String pwdCheck() { return "/board/pwd-check"; }
+
+    @GetMapping("/modify")
+    public String qnaModify() { return "/board/qna-product-modify"; }
+
+    @GetMapping("/review-lists")
+    public String reviewList() { return "/board/review-list"; }
+
+    @GetMapping("/review-write")
+    public String reviewWrite() { return "/board/review-write"; }
+
+    // 리뷰 수정 페이지 구현 x
+ //   @GetMapping("")
+ //   public String reviewModify() { return "/board/review-modify"; }
 
 }
