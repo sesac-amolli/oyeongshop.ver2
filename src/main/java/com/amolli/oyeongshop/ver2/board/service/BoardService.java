7a2d8adc956e925d7f93f30a77352bb63c610820
nayonez <nykim1102@gmail.com>
2023-11-06 14:11:04 +0900

    add CreationTimeStamp

---
+++ src/main/java/com/amolli/oyeongshop/ver2/board/service/BoardService.java
@@ -0,0 +1,7 @@
+package com.amolli.oyeongshop.ver2.board.service;
+
+import com.amolli.oyeongshop.ver2.board.model.Review;
+
+public interface BoardService {
+    Review save(Review review);
+}
--- src/main/java/com/amolli/oyeongshop/ver2/board/service/BoardServiceImpl.java
+++ src/main/java/com/amolli/oyeongshop/ver2/board/service/BoardServiceImpl.java
@@ -1,4 +1,16 @@
 package com.amolli.oyeongshop.ver2.board.service;
 
-public class BoardServiceImpl {
+import com.amolli.oyeongshop.ver2.board.model.Review;
+import com.amolli.oyeongshop.ver2.board.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final ReviewRepository reviewRepository;
+
+    public Review save(Review review) { return reviewRepository.save(review); }
+
 }
