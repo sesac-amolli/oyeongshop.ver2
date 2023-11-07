458f28f10a975dd729aa07c9a11216e5e52206f0
5seung <126163816+5seung@users.noreply.github.com>
2023-11-07 17:00:55 +0900

    securitfeat login, sign-up

--- src/main/java/com/amolli/oyeongshop/ver2/user/model/User.java
+++ src/main/java/com/amolli/oyeongshop/ver2/user/model/User.java
@@ -1,10 +1,10 @@
 package com.amolli.oyeongshop.ver2.user.model;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import net.bytebuddy.asm.Advice;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "tbl_user")
+@DynamicInsert
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User{
@@ -21,6 +22,8 @@ public class User{
     @Id
     private String userId;
 
+    @Column(nullable = false)
+    @ColumnDefault("'USER'")
     private String userGrade;
 
     private String userName;
@@ -29,18 +32,81 @@ public class User{
 
     private String userPhone;
 
+    private String userEmail;
+
+    @Column(nullable = false)
+    @ColumnDefault("'OY'")
+    private String userSnsDist;
+
     @CreationTimestamp
     private LocalDate userRegdate;
 
     private Long userPoint;
 
+    @Column(nullable = false)
+    @ColumnDefault("'YES'") // default
     private String userStatus;
+
     @OneToMany(mappedBy = "user")
     private List<Cart> carts = new ArrayList<>();
+
     @OneToMany(mappedBy = "user")
     private List<UserAddr> userAddrs = new ArrayList<>();
+
     @OneToMany(mappedBy = "user")
     private List<Wishlist> wishlists = new ArrayList<>();
+
     @OneToMany(mappedBy = "user")
     private List<Point> points = new ArrayList<>();
+
+    public void encPwd(String userPwd){
+        this.userPwd = userPwd;
+    }
+
+    public void givePoint(Long point){
+        userPoint=point;
+    }
+
+    public void giveGrade(String grade){
+        userGrade=grade;
+    }
+
+    @Builder
+    public User(String userId, String userGrade, String userName, String userPwd, String userPhone, String userEmail,
+                String userSnsDist, LocalDate userRegdate, Long userPoint, String userStatus, List<Cart> carts,
+                List<UserAddr> userAddrs, List<Wishlist> wishlists, List<Point> points) {
+        this.userId = userId;
+        this.userGrade = userGrade;
+        this.userName = userName;
+        this.userPwd = userPwd;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
+        this.userSnsDist = userSnsDist;
+        this.userRegdate = userRegdate;
+        this.userPoint = userPoint;
+        this.userStatus = userStatus;
+        this.carts = carts;
+        this.userAddrs = userAddrs;
+        this.wishlists = wishlists;
+        this.points = points;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userGrade='" + userGrade + '\'' +
+                ", userName='" + userName + '\'' +
+                ", userPwd='" + userPwd + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", userRegdate=" + userRegdate +
+                ", userPoint=" + userPoint +
+                ", userStatus='" + userStatus + '\'' +
+                ", carts=" + carts +
+                ", userAddrs=" + userAddrs +
+                ", wishlists=" + wishlists +
+                ", points=" + points +
+                '}';
+    }
 }
