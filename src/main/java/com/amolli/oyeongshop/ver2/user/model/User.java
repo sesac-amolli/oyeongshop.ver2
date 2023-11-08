dbad45c08d4961439ee7c0756fa65f91346e6d9a
dev-hubris <147739911+dev-hubris@users.noreply.github.com>
2023-11-08 13:56:26 +0900

    Revert "Create JWT, Security"

--- src/main/java/com/amolli/oyeongshop/ver2/user/model/User.java
+++ src/main/java/com/amolli/oyeongshop/ver2/user/model/User.java
@@ -1,35 +1,25 @@
 package com.amolli.oyeongshop.ver2.user.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.*;
 import net.bytebuddy.asm.Advice;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import org.hibernate.annotations.DynamicInsert;
-
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "tbl_user")
 @DynamicInsert
 @Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor//(access = AccessLevel.PROTECTED)
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User{
 
     @Id
-    @Column(name = "user_id", length = 50)
     private String userId;
 
     @Column(nullable = false)
@@ -37,15 +27,8 @@ public class User {
     private String userGrade;
 
     private String userName;
-  
-    // 23.11.06 스프링 시큐리티 확인용 userPwd > password 로 정정
-    // @JsonIgnore
-    // @Column(name = "password", length = 100)
-    private String userPwd;
 
-    // 23.11.06 스프링 시큐리티 확인용 userEmail > email 로 정정
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
+    private String userPwd;
 
     private String userPhone;
 
@@ -58,19 +41,11 @@ public class User {
     @CreationTimestamp
     private LocalDate userRegdate;
 
-
     private Long userPoint;
 
     @Column(nullable = false)
     @ColumnDefault("'YES'") // default
     private String userStatus;
-
-
-//    @Builder
-//    public User( String email, String password) {
-//        this.email = email;
-//        this.password = password;
-//    }
 
     @OneToMany(mappedBy = "user")
     private List<Cart> carts = new ArrayList<>();
@@ -83,14 +58,6 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Point> points = new ArrayList<>();
-
-   @ManyToMany
-   @JoinTable(
-            name = "user_authority",
-            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
-            inverseJoinColumns = {@JoinColumn(name = "authority_id", referencedColumnName = "authority_id")})
-    private Set<Authority> authorities;
-
 
     public void encPwd(String userPwd){
         this.userPwd = userPwd;
@@ -142,5 +109,4 @@ public class User {
                 ", points=" + points +
                 '}';
     }
-
 }
--- src/main/java/com/amolli/oyeongshop/ver2/user/repository/UserRepository.java
+++ src/main/java/com/amolli/oyeongshop/ver2/user/repository/UserRepository.java
@@ -1,12 +1,7 @@
 package com.amolli.oyeongshop.ver2.user.repository;
 
 import com.amolli.oyeongshop.ver2.user.model.User;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    @EntityGraph(attributePaths = "authorities")
-    Optional<User> findOneWithAuthoritiesByUserId(String userId);
+public interface UserRepository extends JpaRepository<User, String> {
 }
