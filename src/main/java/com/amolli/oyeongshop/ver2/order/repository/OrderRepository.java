8321037ad220b150708368af7036cc79a690ed8f
Yunseol Kim <yseolcoding@gmail.com>
2023-11-09 16:17:12 +0900

    fix : OrderRepository

--- src/main/java/com/amolli/oyeongshop/ver2/order/repository/OrderRepository.java
+++ src/main/java/com/amolli/oyeongshop/ver2/order/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Order findByUserId(int userId);
+    //Order findByUserId(String userId);
 }
