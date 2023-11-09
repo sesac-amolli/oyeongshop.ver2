637475d6fe1bd22c536789bb90d9004110cadca5
JHK <kiko130@naver.com>
2023-11-09 22:32:04 +0900

    Product management, register MVC 구현

--- src/main/java/com/amolli/oyeongshop/ver2/product/controller/ProductController.java
+++ src/main/java/com/amolli/oyeongshop/ver2/product/controller/ProductController.java
@@ -6,11 +6,11 @@ import com.amolli.oyeongshop.ver2.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -19,6 +19,7 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
+    // 상품 리스트
     @GetMapping("/list")
     public String productList(Model model) {
         List<ProductResponse> productList = productService.findProductAll();
@@ -27,6 +28,7 @@ public class ProductController {
         return "product/product-list";
     }
 
+    // 상품 상세정보
     @GetMapping("/detail/{prodId}")
     public ModelAndView productDetail(@PathVariable Long prodId, Model model) {
         // 중복 옵션 제거
@@ -40,13 +42,59 @@ public class ProductController {
         mav.addObject(productService.findById(prodId));
         return mav;
     }
+
+    // 상품 등록 화면으로 렌더링
     @GetMapping("/register")
-    public String productRegister( ) {
+    public String productRegister(Model model) {
+        model.addAttribute("product", Product.builder().build());
         return "product/product-register";
     }
+
+    // 상품 등록 POST 요청을 처리하는 메서드
+    @PostMapping("/register")
+    public String processCreationForm(@Valid Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            return "product/product-register";
+        } else {
+            // productService를 사용하여 상품을 등록
+            productService.save(product);
+
+            // html 파일이 아닌 /product/management 를 redirect해준다.
+            return "redirect:/product/management";
+        }
+    }
+
+    // 상품 관리 리스트
+    @GetMapping("/management")
+    public String productManagement(Model model) {
+        List<ProductResponse> productList = productService.findProductAll();
+
+        model.addAttribute("productList",productList);
+        return "product/product-management";
+    }
+
+//    @GetMapping("/management")
+//    public List<ProductResponse> productManagement(Model model) {
+//        List<ProductResponse> productList = productService.findProductAll();
+//
+//        return productList;
+//    }
+
+//    @PostMapping(value = {"/register"})
+//    public String processCreationForm(@Validated Product product, BindingResult result, @PathVariable Long prodId) {
+//        if (result.hasErrors()) {
+//            return "product/product-register";
+//        }
+//        else {
+//            // OwnerService 생성 후 등록 처리 로직 구현, 완성
+//            Product savedProduct = productService.save(product, prodId);
+//            return "redirect:/product/" + savedProduct.getProdId();
+//        }
+//    }
 
     @GetMapping("/detail/edit")
     public String productDetailEdit( ) {
         return "/product/product-detail-edit";
     }
 }
+
--- src/main/java/com/amolli/oyeongshop/ver2/product/repository/ProductOptionRepository.java
+++ src/main/java/com/amolli/oyeongshop/ver2/product/repository/ProductOptionRepository.java
@@ -2,8 +2,14 @@ package com.amolli.oyeongshop.ver2.product.repository;
 
 import com.amolli.oyeongshop.ver2.product.model.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 //  JPA(Java Persistence API)를 사용하여 데이터베이스와 상호작용하는 데 사용되는 인터페이스
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-//    List<Product> findAll();                                      Entity    pk
+//    List<ProductOption> findAll();                                Entity    pk
+//    @Query(value = "SELECT * FROM tbl_product_option WHERE prod_opt_color = :prodOptColor", nativeQuery = true)
+//    List<ProductOption> productOptionSize(@Param("prodOptColor") String prodOptColor);
 }
